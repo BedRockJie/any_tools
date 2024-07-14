7db9d3c5372c24881cf2bb477e07e9281a9a6fd4
BedRock <49542898+BedRockJie@users.noreply.github.com>
2024-07-14 16:09:14 +0800

    feat: add test user gtest (#3)

--- src/upgrade/upgrade.c
+++ src/upgrade/upgrade.c
@@ -10,6 +10,7 @@
 #include "argparse.h"
 #include <stdio.h>
 
+
 #define PERM_READ  (1<<0)
 #define PERM_WRITE (1<<1)
 #define PERM_EXEC  (1<<2)
@@ -67,4 +68,30 @@ int device_upgrade(int argc, const char** argv)
         printf("perms: %d\n", perms);
     }
 	return 0;
+}
+
+
+bool info_check(int n)
+{
+    // Trivial case 1: small numbers
+    if (n <= 1) return false;
+
+    // Trivial case 2: even numbers
+    if (n % 2 == 0) return n == 2;
+
+    // Now, we have that n is odd and n >= 3.
+
+    // Try to divide n by every odd number i, starting from 3
+    for (int i = 3;; i += 2) {
+        // We only have to try i up to the square root of n
+        if (i > n / i) break;
+
+        // Now, we have i <= n/i < n.
+        // If n is divisible by i, n is not prime.
+        if (n % i == 0) return false;
+    }
+
+    // n has no integer factor in the range (1, n), and thus is prime.
+    return true;
+
 }
--- src/upgrade/upgrade.h
+++ src/upgrade/upgrade.h
@@ -2,12 +2,14 @@
 #define UPGRADE_H
 
 #include <stdint.h>
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
     int device_upgrade(int argc, const char** argv);
-
+    bool info_check(int n);
 #ifdef __cplusplus
 }
 #endif
